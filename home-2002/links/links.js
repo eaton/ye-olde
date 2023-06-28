fcb7d8fb9da8676a629ac8d759878f79b36187ae
Jeff Eaton <jeff@eaton.fyi>
2023-06-28 12:20:14 -0500

    2001 was actually 2002, adding an index

---
+++ home-2002/links/links.js
@@ -0,0 +1,645 @@
+let links = [
+	{
+		"link_id": 1,
+		"title": "Sluggy Freelance",
+		"description": null,
+		"url": "http://www.sluggy.com"
+	},
+	{
+		"link_id": 2,
+		"title": "MacInTouch",
+		"description": null,
+		"url": "http://www.macintouch.com"
+	},
+	{
+		"link_id": 3,
+		"title": "The Hitchhiker's Guide to the Galaxy",
+		"description": null,
+		"url": "http://www.h2g2.com"
+	},
+	{
+		"link_id": 4,
+		"title": "The Macintosh News Network",
+		"description": null,
+		"url": "http://www.macnn.com"
+	},
+	{
+		"link_id": 5,
+		"title": "Apple Macintosh News at MacCentral Online",
+		"description": null,
+		"url": "http://www.maccentral.com"
+	},
+	{
+		"link_id": 6,
+		"title": "Mac OS Rumors",
+		"description": null,
+		"url": "http://www.macosrumors.com"
+	},
+	{
+		"link_id": 7,
+		"title": "AppleInsider - Insider News and Rumors",
+		"description": null,
+		"url": "http://www.appleinsider.com"
+	},
+	{
+		"link_id": 8,
+		"title": "Interface Zen",
+		"description": null,
+		"url": "http://slashdot.org/article.pl?sid=99/11/30/0954216&mode=nocomment"
+	},
+	{
+		"link_id": 9,
+		"title": "The Mac Orchard",
+		"description": null,
+		"url": "http://www.macorchard.com"
+	},
+	{
+		"link_id": 10,
+		"title": "Internet Underground Music Archive",
+		"description": null,
+		"url": "http://www.iuma.com"
+	},
+	{
+		"link_id": 11,
+		"title": "Maps on the Web",
+		"description": null,
+		"url": "http://www.lib.utexas.edu/Libs/PCL/Map_collection/map_sites/map_sites.html"
+	},
+	{
+		"link_id": 12,
+		"title": "Facts About Israel's History",
+		"description": null,
+		"url": "http://www.iclnet.org/pub/resources/text/history/israel.html"
+	},
+	{
+		"link_id": 13,
+		"title": "History of Israeli Wars",
+		"description": null,
+		"url": "http://www.ahavat-israel.com/eretz/wars.html"
+	},
+	{
+		"link_id": 14,
+		"title": "Israeli History FAQ",
+		"description": null,
+		"url": "http://www.dmoz.org/Regional/Middle_East/Israel/History/faq.html"
+	},
+	{
+		"link_id": 15,
+		"title": "The Open Directory Project",
+		"description": null,
+		"url": "http://dmoz.org"
+	},
+	{
+		"link_id": 16,
+		"title": "On-line SQL Tutorial",
+		"description": null,
+		"url": "http://torresoft.netmegs.com"
+	},
+	{
+		"link_id": 17,
+		"title": "Myth Townhall",
+		"description": null,
+		"url": "http://townhall.clanplaid.net"
+	},
+	{
+		"link_id": 18,
+		"title": "TOYWAR",
+		"description": null,
+		"url": "http://www.toywar.com"
+	},
+	{
+		"link_id": 19,
+		"title": "Cyberpunk Information Database",
+		"description": null,
+		"url": "http://www.cyberpunkproject.org/idb"
+	},
+	{
+		"link_id": 20,
+		"title": "Plumb Design Visual Thesaurus",
+		"description": null,
+		"url": "http://www.plumbdesign.com/thesaurus"
+	},
+	{
+		"link_id": 21,
+		"title": "Guide to Cascading Style Sheets",
+		"description": null,
+		"url": "http://www.htmlhelp.com/reference/css"
+	},
+	{
+		"link_id": 22,
+		"title": "The CD Club FAQ",
+		"description": null,
+		"url": "http://www.blooberry.com/cdfaq/index.html"
+	},
+	{
+		"link_id": 23,
+		"title": "PowerBook Motherboard Layouts",
+		"description": null,
+		"url": "http://www.macgurus.com/graphics/mboardpowerbooks.html"
+	},
+	{
+		"link_id": 24,
+		"title": "The CD Club Web Server",
+		"description": null,
+		"url": "http://www.cd-clubs.com"
+	},
+	{
+		"link_id": 25,
+		"title": "Lycos Environment News",
+		"description": null,
+		"url": "http://ens.lycos.com"
+	},
+	{
+		"link_id": 26,
+		"title": "RPGnet - the Inside Scoop on Gaming",
+		"description": null,
+		"url": "http://www.rpg.net"
+	},
+	{
+		"link_id": 27,
+		"title": "Commercial Archive - Television at Its Finest",
+		"description": null,
+		"url": "http://www.commercial-archive.com"
+	},
+	{
+		"link_id": 28,
+		"title": "Ask Tog",
+		"description": null,
+		"url": "http://www.asktog.com"
+	},
+	{
+		"link_id": 29,
+		"title": "Useit.Com",
+		"description": null,
+		"url": "http://www.useit.com"
+	},
+	{
+		"link_id": 30,
+		"title": "Emily Dickinson Poems On-line",
+		"description": null,
+		"url": "http://userweb.interactive.net/~krisxlee/emily/poemsOnline.html"
+	},
+	{
+		"link_id": 31,
+		"title": "icons.cx - desktop pictures",
+		"description": null,
+		"url": "http://www.icons.cx/desktops.html"
+	},
+	{
+		"link_id": 32,
+		"title": "A Rubber Duck to Every Bathroom",
+		"description": null,
+		"url": "http://www.students.tut.fi/~cami/Quack/duckpage.htm"
+	},
+	{
+		"link_id": 33,
+		"title": "Claire Hand's Rubber Duckies",
+		"description": null,
+		"url": "http://www.hand-family.org/Claire/Duckies"
+	},
+	{
+		"link_id": 34,
+		"title": "Rubber Ducky Homepage By Gor Wu",
+		"description": null,
+		"url": "http://www.freenet.hamilton.on.ca/~ab357/Profile.html"
+	},
+	{
+		"link_id": 35,
+		"title": "Luddite Purity Test",
+		"description": null,
+		"url": "http://www.luddites.com/purity.html"
+	},
+	{
+		"link_id": 36,
+		"title": "The Ad Critic - All Ads, All The Time.",
+		"description": null,
+		"url": "http://www.adcritic.com"
+	},
+	{
+		"link_id": 37,
+		"title": "Bradsoft TopStyle",
+		"description": null,
+		"url": "http://www.bradsoft.com/topstyle/index.asp"
+	},
+	{
+		"link_id": 38,
+		"title": "signalgrau | designbureau",
+		"description": null,
+		"url": "http://www.signalgrau.com"
+	},
+	{
+		"link_id": 39,
+		"title": "Animal Pictures Archive",
+		"description": null,
+		"url": "http://www.best5.net/animal"
+	},
+	{
+		"link_id": 40,
+		"title": "The Unbound Bible",
+		"description": null,
+		"url": "http://unbound.biola.edu"
+	},
+	{
+		"link_id": 41,
+		"title": "Bartleby.com: Great Books Online",
+		"description": null,
+		"url": "http://www.bartleby.com"
+	},
+	{
+		"link_id": 42,
+		"title": "The Iconhood - Icons For Your Mac",
+		"description": null,
+		"url": "http://www.iconhood.cjb.net"
+	},
+	{
+		"link_id": 43,
+		"title": "IconTown | Welcome Home",
+		"description": null,
+		"url": "http://www.icontown.de"
+	},
+	{
+		"link_id": 44,
+		"title": "The New Hacker's Dictionary",
+		"description": null,
+		"url": "http://www.tuxedo.org/~esr/jargon/html"
+	},
+	{
+		"link_id": 45,
+		"title": "Complimentary Chromatic Colors  - Menu",
+		"description": null,
+		"url": "http://www.smartpixel.net/chromoweb/uks/indexgb.html"
+	},
+	{
+		"link_id": 46,
+		"title": "Web Colors",
+		"description": null,
+		"url": "http://www.smartpixel.net/chromoweb"
+	},
+	{
+		"link_id": 47,
+		"title": "ASPFree.com: Your 1st source for free ASP code",
+		"description": null,
+		"url": "http://www.aspfree.com"
+	},
+	{
+		"link_id": 48,
+		"title": "The 99.999 Percent Solution",
+		"description": null,
+		"url": "http://www.cio.com/archive/021500_percent.html"
+	},
+	{
+		"link_id": 49,
+		"title": "Etresoft Internet Site",
+		"description": null,
+		"url": "http://www.etresoft.com"
+	},
+	{
+		"link_id": 50,
+		"title": "DevASP for ASP developers",
+		"description": null,
+		"url": "http://www.devasp.com"
+	},
+	{
+		"link_id": 51,
+		"title": "Jakob Nielsen Answers User Questions",
+		"description": null,
+		"url": "http://slashdot.org/article.pl?sid=00/03/03/096223"
+	},
+	{
+		"link_id": 52,
+		"title": "Memex and Beyond Web Site",
+		"description": null,
+		"url": "http://www.cs.brown.edu/memex"
+	},
+	{
+		"link_id": 53,
+		"title": "Barna Research Online - Home.asp",
+		"description": null,
+		"url": "http://www.barna.org/cgi-bin/Home.asp"
+	},
+	{
+		"link_id": 54,
+		"title": "Writing with a byte: Computer creates Fiction",
+		"description": null,
+		"url": "http://cnn.com/2000/books/news/02/29/storytelling.machine.ap/index.html"
+	},
+	{
+		"link_id": 55,
+		"title": "Body-Mind QueenDom",
+		"description": null,
+		"url": "http://www.queendom.com/test_col.html"
+	},
+	{
+		"link_id": 56,
+		"title": "nineinchnails.net: the perfect drug lyrics",
+		"description": null,
+		"url": "http://www.nineinchnails.net/lyricstpd.html"
+	},
+	{
+		"link_id": 57,
+		"title": "Before There Was Rock 'N Roll",
+		"description": null,
+		"url": "http://www.geocities.com/Hollywood/Trailer/7296/index.htm"
+	},
+	{
+		"link_id": 58,
+		"title": "Album Covers",
+		"description": null,
+		"url": "http://covers.virtualave.net"
+	},
+	{
+		"link_id": 59,
+		"title": "MacFixIt.com: Troubleshooting for your Mac",
+		"description": null,
+		"url": "http://www.macfixit.com"
+	},
+	{
+		"link_id": 60,
+		"title": "Decapolis",
+		"description": null,
+		"url": "http://www.decapolis.com"
+	},
+	{
+		"link_id": 61,
+		"title": "PSTRUH Software",
+		"description": null,
+		"url": "http://www.pstruh.cz"
+	},
+	{
+		"link_id": 62,
+		"title": "eMailman(sm) -- electronic superhero(sm)",
+		"description": null,
+		"url": "http://www.emailman.com"
+	},
+	{
+		"link_id": 63,
+		"title": "Classic Macintosh",
+		"description": null,
+		"url": "http://classicmac.iwarp.com/index.html"
+	},
+	{
+		"link_id": 64,
+		"title": "Growing Up With Janeane Garofalo",
+		"description": null,
+		"url": "http://www.homunculus.com/icons/garofalojaneane/articles/GarofaloMC0005.html"
+	},
+	{
+		"link_id": 65,
+		"title": "MSDN Online Library",
+		"description": null,
+		"url": "http://msdn.microsoft.com/library"
+	},
+	{
+		"link_id": 66,
+		"title": "Quinn's Schmoo",
+		"description": null,
+		"url": "http://www.quinnarama.com"
+	},
+	{
+		"link_id": 67,
+		"title": "Decapolis",
+		"description": null,
+		"url": "http://www.decapolis.com"
+	},
+	{
+		"link_id": 68,
+		"title": "Welcome To Walnut Creek Presbyterian Church",
+		"description": null,
+		"url": "http://www.wcpres.org"
+	},
+	{
+		"link_id": 69,
+		"title": "DealMac News",
+		"description": null,
+		"url": "http://www.dealmac.com"
+	},
+	{
+		"link_id": 70,
+		"title": "Electronic Warfare Associates Home Page",
+		"description": null,
+		"url": "http://www.ewa.com"
+	},
+	{
+		"link_id": 71,
+		"title": "Mob Purchasing",
+		"description": null,
+		"url": "http://www.mobbuy.com"
+	},
+	{
+		"link_id": 72,
+		"title": "Doctor Who Cartoons",
+		"description": null,
+		"url": "http://www.cuttingsarchive.org.uk/1999upda.htm"
+	},
+	{
+		"link_id": 73,
+		"title": "Old Newspaper Clippings",
+		"description": null,
+		"url": "http://www.nsn.org/wrkhome/hmca/c_indx.htm"
+	},
+	{
+		"link_id": 74,
+		"title": "Library of Congress Home Page",
+		"description": null,
+		"url": "http://www.loc.gov"
+	},
+	{
+		"link_id": 75,
+		"title": "Religion and the Founding of the American Republic",
+		"description": null,
+		"url": "http://lcweb.loc.gov/exhibits/religion"
+	},
+	{
+		"link_id": 76,
+		"title": "Rare Book and Special Collections Reading Room",
+		"description": null,
+		"url": "http://lcweb.loc.gov/rr/rarebook"
+	},
+	{
+		"link_id": 77,
+		"title": "Browse Library of Congress Subject Guides",
+		"description": null,
+		"url": "http://lcweb.loc.gov/global/subject.html"
+	},
+	{
+		"link_id": 78,
+		"title": "Images of War and Empire",
+		"description": null,
+		"url": "http://www.boondocksnet.com/centennial/image.html"
+	},
+	{
+		"link_id": 79,
+		"title": "Welcome to Dime Novels",
+		"description": null,
+		"url": "http://www-sul.stanford.edu/depts/dp/pennies/home.html"
+	},
+	{
+		"link_id": 80,
+		"title": "American Memory ",
+		"description": null,
+		"url": "http://memory.loc.gov/ammem/ammemhome.html"
+	},
+	{
+		"link_id": 81,
+		"title": "Manliness for Young Men and their Well Wishers",
+		"description": null,
+		"url": "http://moa.umdl.umich.edu/cgi/pageviewer?frames=1&coll=moa&view=75&root=mm000016%2F0357manl%2Fv0000%2Fi000&tif=00030001.tif&cite=http%3A%2F%2Fmoa.umdl.umich.edu%2Fcgi%2Fsgml%2Fmoa-idx%3Fnotisid%3DAJF9985"
+	},
+	{
+		"link_id": 82,
+		"title": "Map Collections Home Page",
+		"description": null,
+		"url": "http://memory.loc.gov/ammem/gmdhtml/gmdhome.html"
+	},
+	{
+		"link_id": 83,
+		"title": "Automatic WWW Text Borkifier",
+		"description": null,
+		"url": "http://www.astro.queensu.ca/~dursi/borker.html"
+	},
+	{
+		"link_id": 84,
+		"title": "Cuba Daily",
+		"description": null,
+		"url": "http://www.cubadaily.com"
+	},
+	{
+		"link_id": 85,
+		"title": "Web Hosting Providers",
+		"description": null,
+		"url": "http://www.hostsearch.com"
+	},
+	{
+		"link_id": 86,
+		"title": "Hobbes' Internet Timeline",
+		"description": null,
+		"url": "http://www.isoc.org/guest/zakon/Internet/History/HIT.html"
+	},
+	{
+		"link_id": 87,
+		"title": "The Wall of Shame",
+		"description": null,
+		"url": "http://www.jiggscasey.com/rjinswand/wallofshame.html"
+	},
+	{
+		"link_id": 88,
+		"title": "Hackers - Q&A with Emmanuel Goldstein",
+		"description": null,
+		"url": "http://www.cnn.com/TECH/specials/hackers/qandas/goldstein.html"
+	},
+	{
+		"link_id": 89,
+		"title": "Who's Who in the History of Mysticism",
+		"description": null,
+		"url": "http://www.clas.ufl.edu/users/gthursby/mys/whoswho.htm"
+	},
+	{
+		"link_id": 90,
+		"title": "Salon Books: Battle without blood",
+		"description": null,
+		"url": "http://www.salon.com/books/int/2000/05/04/ignatieff/index.html"
+	},
+	{
+		"link_id": 91,
+		"title": "Adbusters: How to create your own print ad",
+		"description": null,
+		"url": "http://adbusters.org/spoofads/printad"
+	},
+	{
+		"link_id": 92,
+		"title": "The Sapir-Whorf Hypothesis",
+		"description": null,
+		"url": "http://www.aber.ac.uk/media/Documents/short/whorf.html"
+	},
+	{
+		"link_id": 93,
+		"title": "Spyonit.com",
+		"description": null,
+		"url": "http://www.spyonit.com/Home"
+	},
+	{
+		"link_id": 94,
+		"title": "TidBITS Electronic Publishing",
+		"description": null,
+		"url": "http://www.tidbits.com"
+	},
+	{
+		"link_id": 95,
+		"title": "The Story",
+		"description": null,
+		"url": "http://belps.freewebsites.com/TheStory.htm"
+	},
+	{
+		"link_id": 96,
+		"title": "iconverge - an icon compilation",
+		"description": null,
+		"url": "http://www.madsciencelaboratories.com/iconverge"
+	},
+	{
+		"link_id": 97,
+		"title": "the pseudoroom",
+		"description": null,
+		"url": "http://www.pseudoroom.com"
+	},
+	{
+		"link_id": 98,
+		"title": "SwapIt.com",
+		"description": null,
+		"url": "http://www.swapit.com"
+	},
+	{
+		"link_id": 99,
+		"title": "coolstop",
+		"description": null,
+		"url": "http://www.coolstop.com/v5.2k"
+	},
+	{
+		"link_id": 100,
+		"title": "Mars Hill Forum: Revealing Christ in Culture",
+		"description": null,
+		"url": "http://www.marshillforum.org/mars.html"
+	},
+	{
+		"link_id": 101,
+		"title": "Bidirectional English-Japanese Dictionary",
+		"description": null,
+		"url": "http://205.163.136.51/EJEdic"
+	},
+	{
+		"link_id": 102,
+		"title": "CSS Structure and Rules",
+		"description": null,
+		"url": "http://www.htmlhelp.com/reference/css/structure.html"
+	},
+	{
+		"link_id": 103,
+		"title": "The Socrates Group CSS Test Page",
+		"description": null,
+		"url": "http://www.hpaa.com/css1/styletest.asp"
+	},
+	{
+		"link_id": 104,
+		"title": "Browser Chart",
+		"description": null,
+		"url": "http://hotwired.lycos.com/webmonkey/reference/browser_chart"
+	},
+	{
+		"link_id": 105,
+		"title": "maria's home page",
+		"description": null,
+		"url": "http://www.predicate.net/users/rei/"
+	}
+];
+
+function buildLinks() {
+  let html = "";
+  for (let i = 0; i < links.length; i++) {
+    link = links[i];
+    html += `<h3><a href="${link.url}">${link.title}</a></h3>`;
+    if (link.description !== null) {
+      html += `<p>{link.description}</p>`;
+    }
+  }
+  return html;
+}
+
